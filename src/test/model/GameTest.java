bad6b9a2144b1833cc60d5b6e2cc81f7defafdbd
tim13246879 <timyang13246879@hotmail.com>
2023-03-08 18:54:43 -0800

    added test for testEquals() for code coverage

--- src/test/model/GameTest.java
+++ src/test/model/GameTest.java
@@ -14,14 +14,13 @@ class GameTest {
     Game g;
     Game g1;
     Game g2;
-    Game g3;
+
 
     @BeforeEach
     private void setup() {
         g = new Game();
         g1 = new Game(1, 2, 3, 4, 10, "s1", LocalDateTime.now());
         g2 = new Game(1, 2, 3, 4, 10, "s1", LocalDateTime.now());
-        g3 = new Game(2, 2, 3, 4, 10, "s1", LocalDateTime.now());
     }
 
     // EFFECTS: return true if all numbers in consumed list are in range [1, 13]
@@ -46,9 +45,23 @@ class GameTest {
 
     @Test
     public void testEquals() {
+        Game g3 = new Game(2, 2, 3, 4, 10, "s1", LocalDateTime.now());
+        Game g4 = new Game(1, 5, 3, 4, 10, "s1", LocalDateTime.now());
+        Game g5 = new Game(1, 2, 9, 4, 10, "s1", LocalDateTime.now());
+        Game g6 = new Game(1, 2, 3, 2, 10, "s1", LocalDateTime.now());
+        Game g7 = new Game(1, 2, 3, 4, 10.1, "s1", LocalDateTime.now());
+        Game g8 = new Game(1, 2, 3, 4, 10, "ss1", LocalDateTime.now());
+        Game g9 = new Game(1, 2, 3, 4, 10, "s1", LocalDateTime.of(1, 1, 1, 1, 1));
+
         assertTrue(g1.equals(g1));
         assertTrue(g1.equals(g2));
         assertFalse(g1.equals(g3));
+        assertFalse(g1.equals(g4));
+        assertFalse(g1.equals(g5));
+        assertFalse(g1.equals(g6));
+        assertFalse(g1.equals(g7));
+        assertFalse(g1.equals(g8));
+        assertFalse(g1.equals(g9));
         assertFalse(g1.equals(null));
         assertFalse(g1.equals("Obviously Wrong"));
     }
